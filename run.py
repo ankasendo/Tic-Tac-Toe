31bf0245993eb30f487bd41f7351fdea0fa6e2cd
ankasendo <anka.sendo@gmail.com>
2023-01-02 01:13:22 +0000

    add more methods to tictactoe class

--- run.py
+++ run.py
@@ -52,8 +52,8 @@ class HumanPlayer(Player):
         also checks if the cell on the board has already been used,
         if invalid or already used, will return an error.
         """
-    valid_cell = None
-    value = None
+        valid_cell = None
+        value = None
         while not valid_cell:
             cell = input(self.letter + "'s turn. Input move (0-8):")
             try:
@@ -65,26 +65,58 @@ class HumanPlayer(Player):
                 print("Invalid cell. Try again.")
         return value
 
+
+def mockup_board():
+    """
+    Mockup board to keep visible during the game to facilitate UX
+    """
+    num_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+    for row in num_board:
+        print("| " + " | ".join(row) + " |")
+
+
 class TicTacToe:
     """Class for the main game of Tic Tac Toe"""
-def __init__(self):
-        # single list that represents a 3x3 board, each number = cell
-    self.board = [' ' for _ in range(9)]
-        # keeps track of the winner if there is one
-self.current_winner = None
 
-def print_board(self):
-        # group of three spaces are we choosing row1 = 0,1,2 \ row2 = 3,4,5 \ row3 = 6,7,8 
+ def __init__(self):
+        """
+        Single list that represents a 3x3 board, each number = cell
+        and keeps track of the winner if there is one.
+        """
+        self.board = [' ' for _ in range(9)]
+        self.current_winner = None
+    def print_board(self):
+        """
+        Group of three spaces row1 = 0,1,2 / row2 = 3,4,5 / row3 = 6,7,8
+        and print a vertical line as a separator
+        """
         for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
-            # print a vertical line as a separator
             print("| " + " | ".join(row) + " |")
+
 
 @staticmethod
 def print_board_nums():
-        """Show the number corrispondent to the cells and prints separators"""
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
-        for row in number_board:
+        """
+        Show the number corrispondent to the cells and prints separators
+        """
+        num_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in num_board:
             print("| " + " | ".join(row) + " |")
+    def available_moves(self):
+        """
+        Defines which moves are still available in a list of indeces
+        ["x", "x", "o"] -> [(0, "x"), (1, "x"), (2, "o")]
+        """
+        moves = []
+        for (i, cell) in enumerate(self.board):
+            if cell == " ":
+                moves.append(i)
+        return moves
+    def empty_cells(self):
+        """
+        Returns True if there are empty cells in the board.
+         """
+        return " " in self.board
 
 
 print(TicTacToe().print_board_nums())
